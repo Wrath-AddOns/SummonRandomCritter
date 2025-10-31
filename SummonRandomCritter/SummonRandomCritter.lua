b648d6491bae8a998d0a952164246a06f5ec9df7
eTzmNcbkrng <eTzmNcbkrng@users.noreply.github.com>
2025-10-31 14:17:15 +1300

    Add Flight Point handling
    Now summons a random pet after landing from a Flight Point (Taxi).
    On ChromieCraft `PLAYER_CONTROL_GAINED' and `PLAYER_CONTROL_LOST` are not fired when entering/leaving a Taxi and I think they should be.

--- SummonRandomCritter/SummonRandomCritter.lua
+++ SummonRandomCritter/SummonRandomCritter.lua
@@ -1,9 +1,35 @@
+local inTaxiFlight = false
+local outOfCombat = true
+
 local function OnEvent(self, event, ...)
-	local mounted = IsMounted()
-	if mounted == nil then SummonRandomCritter() end
+    if event == "PLAYER_REGEN_ENABLED" then
+        outOfCombat = true
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        outOfCombat = false
+        return
+    end
+
+    if outOfCombat and event == "UNIT_AURA" and ... == "player" then
+        local onTaxi = UnitOnTaxi("player")
+        local wasOnTaxi = inTaxiFlight
+        inTaxiFlight = (onTaxi == 1)
+
+        if wasOnTaxi and not inTaxiFlight then
+            SummonRandomCritter()
+        end
+    end
+
+    if event == "PLAYER_ENTERING_WORLD" or event == "COMPANION_LEARNED" then
+        if not IsMounted() and not inTaxiFlight then
+            SummonRandomCritter()
+        end
+    end
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("COMPANION_LEARNED")
+f:RegisterEvent("UNIT_AURA")
+f:RegisterEvent("PLAYER_REGEN_ENABLED")
+f:RegisterEvent("PLAYER_REGEN_DISABLED")
 f:SetScript("OnEvent", OnEvent)
